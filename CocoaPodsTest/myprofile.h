53cfa923eba396e75c216b5069d12a7ccc23ad85
shufit <548446009>
2015-05-17 17:18:29 +0800

    add myprofile

---
+++ CocoaPodsTest/myprofile.h
@@ -0,0 +1,13 @@
+//
+//  myprofile.h
+//  CocoaPodsTest
+//
+//  Created by KaiXiao on 15/5/17.
+//  Copyright (c) 2015年 com.youdao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface myprofile : NSObject
+
+@end
---
+++ CocoaPodsTest/myprofile.m
@@ -0,0 +1,13 @@
+//
+//  myprofile.m
+//  CocoaPodsTest
+//
+//  Created by KaiXiao on 15/5/17.
+//  Copyright (c) 2015年 com.youdao. All rights reserved.
+//
+
+#import "myprofile.h"
+
+@implementation myprofile
+
+@end
